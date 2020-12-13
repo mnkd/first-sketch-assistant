cfc524d4cd3149eec54463eff703b12c5dfc8b9f
Mitsuru Nakada <mitsuru.nakada@goodpatch.com>
2020-12-13 22:01:03 +0900

    update rules

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,58 @@
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const textDisallow: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    const { utils } = context
+
+    const pattern = utils.getOption('pattern')
+    if (typeof pattern !== 'string') throw Error()
+  
+    for (const layer of utils.objects.text) {
+      const value = layer.attributedString.string
+      if (value.includes(pattern)) {
+        utils.report(`Layer "${layer.name}" contains "${pattern}"`, layer)
+      }
+    }
   },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'first-sketch-assistant/text-disallow',
+  title: (config) => `Text should not contain "${config.pattern}"`,
+  description: 'Reports a violation when text layers contain a configurable text pattern',
+}
+
+const texFontName: RuleDefinition = {
+  rule: async (context) => {
+    const { utils } = context
+
+    const allowed = utils.getOption('allowed')
+    if (typeof allowed !== 'string') throw Error()
+    
+    for (const text of utils.objects.text) {
+      const fontName = text.style?.textStyle?.encodedAttributes?.MSAttributedStringFontAttribute.attributes.name
+
+      if (!fontName?.startsWith(allowed)) {
+        utils.report(`Invalid font name "${fontName}"`, text)
+      }
+    }
+  },
+  name: 'first-sketch-assistant/text-font-name',
+  title: (config) => `Font name should be start "${config.allowed}"`,
+  description: 'Reports a violation when text layers contain a configurable text pattern',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorld],
+    name: 'first-sketch-assistant',
+    rules: [textDisallow, texFontName],
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'first-sketch-assistant/text-disallow': { 
+          active: true,
+          pattern: 'lorem-ipsum',
+        },
+        'first-sketch-assistant/text-font-name': { 
+          active: true,
+          allowed: 'Hiragino',
+        },
       },
     },
   }
